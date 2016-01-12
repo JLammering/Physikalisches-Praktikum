ad33aff8b5b2ff4776da5fadfe5865e62a185eea
Timo Gräßer <timo.graesser@gmx.de>
2016-01-12 11:27:12 +0100

    jkdböakb ä

--- 11 Viskosimeter/plotappkonst.py
+++ 11 Viskosimeter/plotappkonst.py
@@ -43,8 +43,9 @@ n = ufloat(0.001209, 0.000012)
 #Apparaturkonstante:
 K = n/(T*(p-P))
 print('Apparaturkonstante:', K)
-
 #Reynolds-Zahl:
 l = 0.1
-Re = p*2*r*l/(T*n)
+v = l/T
+d = 2*r
+Re = (p*v*d)/n
 print('Reynolds-Zahl:', Re)
