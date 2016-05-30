4f8c1a9666c78f22a0bd16460e74e4236494e6a2
Jasper Lammering <jlammering@me.com>
2016-05-30 15:25:41 +0200

    Hennings Lieblingsplot und die coole Formel von Timo

--- 20 Geiger-Mueller-Zaehlrohr/ladungsmenge.py
+++ 20 Geiger-Mueller-Zaehlrohr/ladungsmenge.py
@@ -1,6 +1,8 @@
 import numpy as np
 from uncertainties import ufloat
 import uncertainties.unumpy as unp
+import matplotlib.pyplot as plt
+
 
 e = ufloat(1.6021766208e-19, 0.0000000098e-19)
 
@@ -16,3 +18,16 @@ print(I,Nerr,delQ)
 
 np.savetxt('build/ladungsmengeergebnisse.txt', np.column_stack([spannung1, unp.nominal_values(I), unp.nominal_values(delQ), unp.std_devs(delQ)]),
 header="Spannung, stromstaerke, Ladung, Fehler Ladung")
+delQplot = unp.nominal_values(delQ)/(10**9)
+delQploterror = unp.std_devs(delQ)/(10**9)
+
+#plotten
+plt.errorbar(spannung1, delQplot, yerr = delQploterror, fmt='x', label = 'Messwerte mit Fehlerbalken')
+plt.plot(spannung1, delQplot, 'kx')
+plt.xlabel(r'$U \:/\: \si{\volt}$')
+plt.ylabel(r'$\Delta Q \:/\: 10^9\si{\elementarycharge}$')
+plt.legend(loc='best')
+
+
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+plt.savefig('build/ladungsmenge.pdf')
--- 20 Geiger-Mueller-Zaehlrohr/totzeit.py
+++ 20 Geiger-Mueller-Zaehlrohr/totzeit.py
@@ -1,5 +1,7 @@
 import numpy as np
 from uncertainties import ufloat
+import uncertainties.unumpy as unp
+
 
 
 #10s
@@ -22,10 +24,14 @@ N1 = ufloat(N1mess/60, np.sqrt(N1mess)/60)
 N2 = ufloat(N2mess/60, np.sqrt(N2mess)/60)
 N12 = ufloat(N12mess/60, np.sqrt(N12mess)/60)
 
+def totzeit(N1, N2, N12):
+    T = (1/N12)-unp.sqrt((1/(N12**2))-((N1 + N2 - N12)/(N1*N2*N12)))
+    return T
+
 T60 = (N1 + N2 - N12)/(2*N1*N2)
 print('Messung bei 60s: ')
 print('N1 = ', N1, 'N2 = ',N2, 'N12 = ',N12, 'T60 = ',T60)
-
+print('Timos: ', totzeit(N1, N2, N12))
 #Mittelwert
 T = (1/2)*(T10+T60)
 print('T = ',T)
